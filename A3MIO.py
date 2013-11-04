5334170847dc76c6ab5e1e33904adb6c158f3fa0
Stefan Seemayer <stefan@seemayer.de>
2013-11-04 10:20:53 +0100

    Code cleanups

--- A3MIO.py
+++ A3MIO.py
@@ -23,91 +23,121 @@ from Bio.SeqRecord import SeqRecord
 import collections
 import re
 
-def pad(x, l):
-    """Pad x with '-' characters until it has length l"""
-    return x + "-" * (l - len(x))
+def pad(x, l, pad_char="-"):
+    """Pad x with pad_char characters until it has length l"""
+    return x + pad_char * (l - len(x))
+
+
+def insert_gaps(sequences):
+    """Return a iterator over sequences that corresponds to the input sequences with gaps added"""
+
+    re_match = re.compile(r'([A-Z0-9~-])')
+
+    # split input sequences into columns of single-character match (upper case and -) states and
+    # variable-length inserts (lower case)
+    inserts = [
+        re_match.split(seq)
+        for seq in sequences
+    ]
+
+    # for each column, determine maximum length
+    insert_max_lengths = [
+        max(
+            len(inserts[i][j])
+            for i in range(len(inserts))
+        )
+        for j in range(len(inserts[0]))
+    ]
+
+    return (
+        "".join(
+            pad(insert, insert_len)
+            for insert, insert_len in zip(seq, insert_max_lengths)
+        )
+        for seq in inserts
+    )
 
 
 def A2MA3MIterator(format='a3m', remove_inserts=False):
+    """Create a SeqIO-style iterator from parameters
+
+    Arguments:
+        format          -- What input format to parse ('a3m' or 'a2m') (default: 'a3m')
+        remove_inserts  -- Whether inserts with respect to the query sequence should be removed (default: False)
+
+    """
     def inner_iterator(handle, alphabet=single_letter_alphabet, title2ids=None):
         """Generator function to iterate over a3m records (as SeqRecord objects).
 
-        handle - input file
-        alphabet - optional alphabet
-        title2ids - A function that, when given the title of the FASTA file (without the beginning >),
-                    will return the id, name and description (in that order) for the record as a tuple
-                    of strings. If this is not given, then the entire title line will be used as the
-                    description, and the first word as the id and name.
+        Arguments:
+            handle      -- input file
+            alphabet    -- optional alphabet
+            title2ids   -- A function that, when given the title of the FASTA file (without the beginning >),
+                           will return the id, name and description (in that order) for the record as a tuple
+                           of strings. If this is not given, then the entire title line will be used as the
+                           description, and the first word as the id and name.
         """
 
-        re_match = re.compile(r'([A-Z0-9~-])')
         re_insert = re.compile(r'[a-z.]')
 
         if title2ids is None:
             title2ids = lambda t: (t.split(None, 1)[0], t.split(None, 1)[0], t)
 
+
+        # piggyback on the fasta parser for splitting file into title and sequence
+        parsed = collections.OrderedDict(SimpleFastaParser(handle))
+
+        titles = parsed.keys()
+        sequences = parsed.values()
+
         if format == 'a3m' and not remove_inserts:
-
-            # piggyback on the fasta parser for splitting file into title and sequence
-            parsed = collections.OrderedDict(SimpleFastaParser(handle))
-
-            # split input sequences into columns of single-character match (upper case and -) states and
-            # variable-length inserts (lower case)
-            inserts = [
-                re_match.split(seq)
-                for seq in parsed.values()
-            ]
-
-            # for each column, determine maximum length
-            insert_max_lengths = [
-                max(
-                    len(inserts[i][j])
-                    for i in range(len(inserts))
-                )
-                for j in range(len(inserts[0]))
-            ]
-
-            # pad so that every element of a column has the same length
-            for title, seq in zip(parsed.keys(), inserts):
-
-                seq_padded = "".join(
-                    pad(insert, insert_len)
-                    for insert, insert_len in zip(seq, insert_max_lengths)
-                )
-
-                id, name, description = title2ids(title)
-                yield SeqRecord(Seq(seq_padded, alphabet), id=id, name=name, description=description)
+            sequences = insert_gaps(sequences)
 
         elif format == 'a2m' and not remove_inserts:
+            sequences = (
+                seq.replace(".", "-")
+                for seq in sequences
+            )
 
-            for title, seq in SimpleFastaParser(handle):
-                seq = seq.replace(".", "-")
-
-                id, name, description = title2ids(title)
-                yield SeqRecord(Seq(seq, alphabet), id=id, name=name, description=description)
+        elif remove_inserts:
+            sequences = (
+                re_insert.sub('', seq)
+                for seq in sequences
+            )
 
         else:
+            raise ValueError("Unknown format: {0}".format(format))
 
-            for title, seq in SimpleFastaParser(handle):
-                seq_noinserts = re_insert.sub('', seq)
+        for title, seq in zip(titles, sequences):
 
-                id, name, description = title2ids(title)
-                yield SeqRecord(Seq(seq_noinserts, alphabet), id=id, name=name, description=description)
+            id, name, description = title2ids(title)
+            yield SeqRecord(Seq(seq, alphabet), id=id, name=name, description=description)
 
     return inner_iterator
 
-# Monkey patch the Bio.SeqIO iterator registry
-Bio.SeqIO._FormatToIterator['a3m'] = A2MA3MIterator('a3m', False)
-Bio.SeqIO._FormatToIterator['a3m-nogaps'] = A2MA3MIterator('a3m', True)
-Bio.SeqIO._FormatToIterator['a2m'] = A2MA3MIterator('a2m', False)
-Bio.SeqIO._FormatToIterator['a2m-nogaps'] = A2MA3MIterator('a2m', True)
+
+def monkey_patch(name, iterator):
+    """Monkey patch the Bio.SeqIO iterator registry if no such iterator exists yet"""
+
+    if name not in Bio.SeqIO._FormatToIterator:
+        Bio.SeqIO._FormatToIterator[name] = iterator
+
+monkey_patch('a3m', A2MA3MIterator('a3m', False))
+monkey_patch('a3m-nogaps', A2MA3MIterator('a3m', True))
+monkey_patch('a2m', A2MA3MIterator('a2m', False))
+monkey_patch('a2m-nogaps', A2MA3MIterator('a2m', True))
+
 
 if __name__ == '__main__':
 
     def assert_equal(iter_a, iter_b, msg):
         for rec_a, rec_b in zip(iter_a, iter_b):
+            # assert that string representations of SeqRecords match - this is
+            # quite hacky and should not be done :)
             assert repr(rec_a) == repr(rec_b), msg + ": " + repr((rec_a, rec_b))
 
+
+    # Compare our parser against output of reformat.pl from the HH-suite
 
     a2m = list(Bio.SeqIO.parse("data/test.a2m", "a2m"))
     a3m = list(Bio.SeqIO.parse("data/test.a3m", "a3m"))
---
+++ README.md
@@ -0,0 +1,30 @@
+# A3M / A2M input for BioPython
+
+This module enables BioPython to parse the Soeding lab's custom HH-suite alignment formats A3M and A2M. The goal of the module is to replicate the results of the reformat.pl included in the HH-suite as faithfully as possible
+
+## Usage
+
+Simply `import A3MIO` in addition to your BioPython modules. This creates support for the following new formats in both `SeqIO` and `AlignIO` packages:
+
+	* `a3m` - A3Ms with insertions
+	* `a3m-nogaps` A3Ms that ignore insertions w.r.t. query sequence
+	* `a2m` - A2Ms with insertions
+	* `a2m-nogaps` A2Ms that ignore insertions w.r.t. query sequence
+
+
+Example:
+
+	import Bio.SeqIO
+	import A3MIO
+
+	for record in Bio.SeqIO.parse("example.a3m", "a3m"):
+		print(record)
+
+## Status
+Not yet tested on larger sequence databases for accuracy or performance. Be sure to evaluate before you use it :)
+
+## License
+GNU GPL v3
+
+
+
