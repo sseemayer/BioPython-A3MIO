97a604ae4888a8ebd0f568547958adda2b2b1679
Stefan Seemayer <stefan@seemayer.de>
2013-12-19 09:57:08 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='A3MIO',
+    version='0.0.1',
+    description='A3M/A2M I/O for BioPython',
+    author='Stefan Seemayer',
+    author_email='mail@semicolonsoftware.de',
+    url='https://github.com/sseemayer/BioPython-A3MIO',
+    py_modules=['A3MIO'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Bio-Informatics"
+    ]
+)
